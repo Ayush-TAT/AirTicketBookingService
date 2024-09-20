2d51e0a6f42af8a685f00b787b6086b529cee02c
Ayush Sharma <ayushsharma.jamshedpur@gamil.com>
2024-09-20 19:03:32 +0530

    commit

---
+++ BookingService/src/repository/booking-repository.js
@@ -0,0 +1,47 @@
+const {Booking} = require('../models/index');
+const {StatusCodes} = require('http-status-codes');
+const {ValidationError, AppError} = require('./../utils/errors/index');
+
+
+class BookingRepository{
+    async create(data){
+        try {
+            const booking = await Booking.create(data);
+            return booking;
+        } catch (error) {
+            if(error.name== 'SequelizeValidationError'){
+                throw new ValidationError(error)
+            }
+            throw new AppError('RepositoryError',
+            'Cannot Create Booking',
+            'There was some issue creating the booking, please try again later',
+            StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async update(bookingId,data){
+        try {
+            // await Booking.update(data,{
+            //     where : {
+            //         id : bookingId
+            //     }
+            // })
+
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status){
+                booking.status = data.status
+            }
+            await booking.save();
+
+            return booking;
+        } catch (error) {
+            throw new AppError('RepositoryError',
+            'Cannot Updatw Booking',
+            'There was some issue updating the booking, please try again later',
+            StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+      
+    }
+}
+
+module.exports = BookingRepository;
